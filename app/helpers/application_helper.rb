c06f13b869678c7789b8d4a7c72c7b4a979e2e1a
Zhikai Li <desert_eagle1985@hotmail.com>
2011-06-18 13:19:04 -0400

    User 1st draft

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	#return a titile on a per-page basis
+	def title 
+		base_title = "Zhikai Rails Exercise"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title}|#{@title}"
+		end
+	end
+
+	def userName
+		user_name = "Zhikai I love You"
+		"#{user_name}"
+	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -6,6 +8,19 @@ SampleApp::Application.routes.draw do
   get "pages/about"
 
   get "pages/help"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
+  match '/home',    :to => 'pages#home'
+
+
+  root :to => 'pages#home'
+
+  #match '/weibo',    :to => 'pages#www.google.ca'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
